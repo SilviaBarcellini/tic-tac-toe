e7f5270015cc5847844babdcf2d65ff6463bd872
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-02-16 14:47:00 +0100

    refactor code Lifting State Up, Again

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,51 +1,49 @@
-import { useState } from "react";
+//import { useState } from "react";
+import { type } from "os";
 import styles from "../styles/Board.module.css";
 import Square from "./Square";
 
 //9 squares filled with null
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true); //set x next, when not x is next
+type Props = {
+  squares: string[];
+  onClick(i: number): void;
+};
+export default function Board(props: Props) {
+  //these two constant are now passed to the game.tsx
+  //const [squares, setSquares] = useState(Array(9).fill(null));
+  //const [xIsNext, setXIsNext] = useState(true); //set x next, when not x is next
 
-  function handleClick(i) {
-    //the square is already taken, it is not possible to change your mind
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    //when click the squares can be filled wit x or with o
-    const newSquares = squares.slice();
-    newSquares[i] = xIsNext ? "X" : "O";
-    setSquares(newSquares);
-    setXIsNext(!xIsNext);
-  }
+  //handle click fx moved to game.tsx
 
-  function renderSquare(i) {
+  function renderSquare(i: number) {
     //combo of handleClick and returns the new value
-    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
+    return <Square value={props.squares[i]} onClick={() => props.onClick(i)} />;
   }
 
-  const winner = calculateWinner(squares);
+  //??????WEG!!!const status = "Next player: " + (xIsNext ? "X" : "O");
+
+  /* const winner = calculateWinner(squares);
   let status;
   if (winner) {
     status = "Winner " + winner; //game over, no more moves are possible
   } else {
-    status = "Next player: " + (xIsNext ? "X" : "O"); //game goes on, next move
-  }
-
+    status = "Next player: " + (xIsNext ? "X" : "O"); //game goes on, next move($=leon'ssoltuion #literals)
+  } 
+ */
   return (
     <div>
-      <div className={styles.status}>{status}</div>
-      <div className={styles.boardRow}>
+      {/* { <div className={styles.status}>{status}</div> */}
+      <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
         {renderSquare(2)}
       </div>
-      <div className={styles.boardRow}>
+      <div className="board-row">
         {renderSquare(3)}
         {renderSquare(4)}
         {renderSquare(5)}
       </div>
-      <div className={styles.boardRow}>
+      <div className={"board-row"}>
         {renderSquare(6)}
         {renderSquare(7)}
         {renderSquare(8)}
@@ -54,24 +52,4 @@ export default function Board() {
   );
 }
 
-//when every square filled is, then this function starts
-function calculateWinner(squares) {
-  const lines = [
-    //every possible solution list
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+//function calculateWinner(squares) moved to game.tsx
--- components/Game.tsx
+++ components/Game.tsx
@@ -1,16 +1,70 @@
 import styles from "../styles/Game.module.css";
 import Board from "./Board";
+import { useState } from "react";
 
 export default function Game() {
+  //lift up again (same as the board but applied to the game)
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  //function handleClick moved from board.tsx
+  function handleClick(i) {
+    //the square is already taken, it is not possible to change your mind
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    //when click the squares can be filled wit x or with o
+    //const newSquares = squares.slice();
+    //newSquares[i] = xIsNext ? "X" : "O"; //inline condition
+    squares[i] = xIsNext ? "X" : "O";
+    //setSquares(newSquares);
+    setHistory(history.concat([{ squares: squares }]));
+    setXIsNext(!xIsNext); ///!=false so it is 0 turn
+  }
+
+  const current = history[history.length - 1];
+  const winner = calculateWinner(current.squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <div className={styles.game}>
       <div className={styles.gameBoard}>
-        <Board />
+        <Board squares={current.squares} onClick={(i) => handleClick(i)} />
       </div>
       <div className={styles.gameInfo}>
-        <div>{/* status */}</div>
+        <div>{status}</div>
         <ol>{/* TODO */}</ol>
       </div>
     </div>
   );
+}
+
+//moved from board.tsx
+//when every square filled is, then this function starts
+function calculateWinner(squares) {
+  const lines = [
+    //every possible solution list
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
