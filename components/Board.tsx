242c6185fcad631e2cc6f23f3de3ac12f17fe704
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-02-16 10:55:42 +0100

    ✨  add declaring a winner

--- components/Board.tsx
+++ components/Board.tsx
@@ -2,20 +2,35 @@ import { useState } from "react";
 import styles from "../styles/Board.module.css";
 import Square from "./Square";
 
+//9 squares filled with null
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true); //set x next, when not x is next
 
   function handleClick(i) {
+    //the square is already taken, it is not possible to change your mind
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    //when click the squares can be filled wit x or with o
     const newSquares = squares.slice();
-    newSquares[i] = "X";
+    newSquares[i] = xIsNext ? "X" : "O";
     setSquares(newSquares);
+    setXIsNext(!xIsNext);
   }
 
   function renderSquare(i) {
+    //combo of handleClick and returns the new value
     return <Square value={squares[i]} onClick={() => handleClick(i)} />;
   }
 
-  const status = "Next player: X";
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner " + winner; //game over, no more moves are possible
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O"); //game goes on, next move
+  }
 
   return (
     <div>
@@ -37,4 +52,26 @@ export default function Board() {
       </div>
     </div>
   );
+}
+
+//when every square filled is, then this function starts
+function calculateWinner(squares) {
+  const lines = [
+    //every possible solution list
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
